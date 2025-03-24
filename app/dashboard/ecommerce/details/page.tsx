cfbfd268f8607a50ee9f977774686aaaae1b7556
muritador5050 <muritador5050@gmail.com>
2025-03-24 18:36:53 +0100

    i added usePathname and useSearchParams

--- app/dashboard/ecommerce/details/page.tsx
+++ app/dashboard/ecommerce/details/page.tsx
@@ -1,5 +1,79 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import { Box, Text, Image, Stack } from '@chakra-ui/react';
+import { useSearchParams } from 'next/navigation';
 
-export default function details() {
-  return <div>details</div>;
+import { Product } from '@/lib/utils';
+import { UnicodeStarRating } from '@/components/TableComponent';
+
+export default function Details() {
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
+
+  useEffect(() => {
+    if (!id) {
+      setLoading(false);
+      return;
+    }
+
+    async function fetchProduct() {
+      setLoading(true);
+      setError(null);
+      try {
+        const request = await fetch(`https://dummyjson.com/products/${id}`);
+        if (!request.ok) {
+          throw new Error('Failed to fetch product data');
+        }
+        const response: Product = await request.json();
+        setProduct(response);
+      } catch (err) {
+        setError(err + 'An error occurred');
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProduct();
+  }, [id]);
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text color='red'>{error}</Text>;
+  }
+
+  if (!product) {
+    return <Text>Product not found.</Text>;
+  }
+
+  return (
+    <div>
+      <Box maxW='700px' mx='auto' p={5}>
+        <Stack spacing={2}>
+          <Image
+            src={product.images[0]}
+            alt={product.title}
+            objectFit='cover'
+            width='100%'
+            borderTopRadius='2xl'
+            background='whitesmoke'
+          />
+          <Text px={3} fontWeight='bold' fontSize='lg'>
+            {product.title}
+          </Text>
+          <Text px={3}>Category: {product.category}</Text>
+          <Text px={3}>Gender: {product.brand}</Text>
+          <Text px={3}>Price: ${product.price}</Text>
+          <Text px={3}>
+            Review: <UnicodeStarRating rating={product.rating} />
+          </Text>
+        </Stack>
+      </Box>
+    </div>
+  );
 }
--- app/dashboard/ecommerce/shop/page.tsx
+++ app/dashboard/ecommerce/shop/page.tsx
@@ -3,7 +3,6 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { Dot, TableOfContents } from 'lucide-react';
 import {
   useDisclosure,
-  Link,
   Drawer,
   DrawerBody,
   DrawerHeader,
@@ -18,20 +17,28 @@ import {
   Box,
   Image,
 } from '@chakra-ui/react';
+import Link from 'next/link';
 import Search from '@/components/Search';
 import { Product } from '@/lib/utils';
 import { UnicodeStarRating } from '@/components/TableComponent';
-
+import { usePathname } from 'next/navigation';
 export default function Shop() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [selectedByPrice, setSelectedByPrice] = useState('all');
   const [selectedCategory, setSelectedCategory] = useState('all');
   const [products, setProducts] = useState<Product[]>([]);
   const [searchItem, setSearchItem] = useState('');
+  const pathname = usePathname();
+
+  // const searchParams = useSearchParams();
+  // const productId = searchParams.get('id');
+  //Reset function
   function handleReset() {
     setSelectedCategory('all');
     setSelectedByPrice('all');
   }
+
+  //SearchInput function
   const handleOnchange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const search = e.target.value;
@@ -50,6 +57,7 @@ export default function Shop() {
     setProducts(filteredProducts);
   };
 
+  //Fetch products
   const fetchProductsByCategory = async (category: string) => {
     try {
       const url =
@@ -72,6 +80,7 @@ export default function Shop() {
     }
   };
 
+  //Filter products by price
   const filterProductsByPrice = (products: Product[], priceRange: string) => {
     if (priceRange === 'all') return products;
 
@@ -87,12 +96,12 @@ export default function Shop() {
     });
   };
 
+  //SearchQuery
   const searchProductByCategory = useCallback(
     async (category: string, searchQuery: string) => {
       try {
         const allProducts = await fetchProductsByCategory(category);
 
-        // Filter by product title (not category)
         return allProducts.filter((product) =>
           product.title.toLowerCase().includes(searchQuery.toLowerCase())
         );
@@ -104,6 +113,7 @@ export default function Shop() {
     []
   );
 
+  //Effect
   useEffect(() => {
     const fetchAndSearchProducts = async () => {
       if (!searchItem) {
@@ -125,7 +135,7 @@ export default function Shop() {
     <div>
       <p className='text-3xl'>Shop</p>
       <div className='flex items-center'>
-        <Link href='/dashboard' _hover={{ color: 'blue.500' }}>
+        <Link href='/dashboard' className='hover:text-blue-400'>
           Home
         </Link>
         <Dot size={28} />
@@ -135,20 +145,69 @@ export default function Shop() {
         <>
           <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
             <DrawerOverlay />
-            <DrawerContent>
+            <DrawerContent className='bg-custom-bg text-custom-color'>
               <DrawerCloseButton />
               <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
               <DrawerBody>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+                <div className='flex flex-col gap-7 p-5'>
+                  <section className='flex flex-col gap-5 '>
+                    <h1>By Category</h1>
+                    <RadioGroup
+                      onChange={setSelectedCategory}
+                      value={selectedCategory}
+                    >
+                      <Stack spacing={3}>
+                        <Radio value='all'>All</Radio>
+                        <Radio value='beauty'>Beauty</Radio>
+                        <Radio value='skin-care'>Skin-care</Radio>
+                        <Radio value='fragrances'>Fragrances</Radio>
+                        <Radio value='groceries'>Groceries</Radio>
+                        <Radio value='furniture'>Furniture</Radio>
+                        <Radio value='smartphones'>Smartphones</Radio>
+                        <Radio value='mens-shirts'>Mens-shirt</Radio>
+                        <Radio value='womens-dresses'>Womens-shirt</Radio>
+                        <Radio value='sports-accessories'>
+                          Sport-accessories
+                        </Radio>
+                      </Stack>
+                    </RadioGroup>
+                  </section>
+                  <section className='border-b-4 border-white'>
+                    <h1>By Pricing</h1>
+                    <RadioGroup
+                      onChange={setSelectedByPrice}
+                      value={selectedByPrice}
+                    >
+                      <Stack spacing={3}>
+                        <Radio value='all'>All</Radio>
+                        <Radio value='0-15'>0-15</Radio>
+                        <Radio value='15-50'>15-50</Radio>
+                        <Radio value='50-100'>50-100</Radio>
+                        <Radio value='Over100'>Over100</Radio>
+                      </Stack>
+                    </RadioGroup>
+                  </section>
+                  <section className='my-3'>
+                    <div className='flex items-center gap-3 break-all'>
+                      <div className='w-7 h-7 rounded-full bg-red-500'></div>
+                      <div className='w-7 h-7 rounded-full bg-blue-500'></div>
+                      <div className='w-7 h-7 rounded-full bg-purple-500'></div>
+                      <div className='w-7 h-7 rounded-full bg-green-500'></div>
+                      <div className='w-7 h-7 rounded-full bg-yellow-500'></div>
+                      <div className='w-7 h-7 rounded-full bg-brown-500'></div>
+                    </div>
+                  </section>
+                  <Button colorScheme='cyan' onClick={handleReset}>
+                    Reset filter
+                  </Button>
+                </div>
               </DrawerBody>
             </DrawerContent>
           </Drawer>
         </>
-        <div className='flex p-5 bg-custom-bg rounded-xl'>
-          <div className='flex flex-col gap-5  border-r-4 border-purple-500 max-[980px]:hidden grow-1'>
-            <section className='flex flex-col gap-5 border-b-4 border-white'>
+        <div className='flex gap-3 bg-custom-bg rounded-xl'>
+          <div className='flex flex-col gap-7 p-5  max-[980px]:hidden grow-1'>
+            <section className='flex flex-col gap-5 '>
               <h1>By Category</h1>
               <RadioGroup
                 onChange={setSelectedCategory}
@@ -159,6 +218,7 @@ export default function Shop() {
                   <Radio value='beauty'>Beauty</Radio>
                   <Radio value='skin-care'>Skin-care</Radio>
                   <Radio value='fragrances'>Fragrances</Radio>
+                  <Radio value='groceries'>Groceries</Radio>
                   <Radio value='furniture'>Furniture</Radio>
                   <Radio value='smartphones'>Smartphones</Radio>
                   <Radio value='mens-shirts'>Mens-shirt</Radio>
@@ -179,8 +239,7 @@ export default function Shop() {
                 </Stack>
               </RadioGroup>
             </section>
-            <section className='border-b-4 border-white'>
-              <h1>By Color</h1>
+            <section className='my-3'>
               <div className='flex items-center gap-3 break-all'>
                 <div className='w-7 h-7 rounded-full bg-red-500'></div>
                 <div className='w-7 h-7 rounded-full bg-blue-500'></div>
@@ -194,7 +253,7 @@ export default function Shop() {
               Reset filter
             </Button>
           </div>
-          <div className=' grow-7'>
+          <div className=' grow-7 p-5'>
             <div className='flex justify-between items-center mb-7'>
               <span onClick={onOpen} className='min-[980px]:hidden'>
                 <TableOfContents />
@@ -206,38 +265,58 @@ export default function Shop() {
                 onChange={handleOnchange}
               />
             </div>
-            <div className='grid grid-cols-2 gap-3'>
+            <div className='min-[980px]:grid grid-cols-2 gap-5 '>
               {products.length === 0 ? (
                 <Text className='text-center'>No item found</Text>
               ) : (
                 <>
-                  {products.map((product) => (
-                    <Box
-                      key={product.id}
-                      borderWidth='1px'
-                      borderRadius='lg'
-                      padding='4'
-                      marginBottom='2'
-                    >
-                      <Stack spacing={2}>
-                        <Image
-                          src={product.images[0]}
-                          alt='product'
-                          objectFit='cover'
-                          boxSize='300px'
-                        />
-                        <Text fontWeight='bold' fontSize='lg'>
-                          {product.title}
-                        </Text>
-                        <Text>Category: {product.category}</Text>
-                        <Text>Gender: {product.brand}</Text>
-                        <Text>Price: ${product.price}</Text>
-                        <Text>
-                          Review: <UnicodeStarRating rating={product.rating} />
-                        </Text>
-                      </Stack>
-                    </Box>
-                  ))}
+                  {products.map((product) => {
+                    // const productURL = `${pathname}/details?id=${productId}`;
+
+                    return (
+                      <Box
+                        key={product.id}
+                        sx={{
+                          transition: 'transform 0.2s ease-in-out',
+                          _hover: { transform: 'translateY(-10px)' },
+                        }}
+                        bg='rgb(17, 28, 45)'
+                        boxShadow='0 10px 1em black'
+                        borderRadius='2xl'
+                        paddingBottom={3}
+                        cursor='pointer'
+                      >
+                        <Stack spacing={2}>
+                          <Link
+                            href={{
+                              pathname: `${pathname}/details`,
+                              query: { id: product.id }, // Correct way to pass the ID
+                            }}
+                          >
+                            <Image
+                              src={product.images[0]}
+                              alt='product'
+                              objectFit='cover'
+                              width='100%'
+                              maxH='50vh'
+                              borderTopRadius='2xl'
+                              background='whitesmoke'
+                            />
+                          </Link>
+                          <Text px={3} fontWeight='bold' fontSize='lg'>
+                            {product.title}
+                          </Text>
+                          <Text px={3}>Category: {product.category}</Text>
+                          <Text px={3}>Gender: {product.brand}</Text>
+                          <Text px={3}>Price: ${product.price}</Text>
+                          <Text px={3}>
+                            Review:{' '}
+                            <UnicodeStarRating rating={product.rating} />
+                          </Text>
+                        </Stack>
+                      </Box>
+                    );
+                  })}
                 </>
               )}
             </div>
