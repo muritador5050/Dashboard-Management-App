f8b75caa915b0f179d4521dc8513bea67fc53d8e
muritador5050 <muritador5050@gmail.com>
2025-03-30 17:38:57 +0100

    i changed navbar z-index value from 2000 to 1000

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -22,9 +22,10 @@ export default function Navbar() {
   const { collapse, handleCollapse, handleToggle, toggleSiderbarWidth } =
     useNav();
   const [swipeModal, setSwipeModal] = useState(false);
+  const [search, setSearch] = useState('');
   return (
     <>
-      <nav className=' bg-custom-bg p-5 sticky top-0 right-0 z-2000  text-custom-color rounded-2xl m-[1em] text-2xl cursor-pointer'>
+      <nav className=' bg-custom-bg p-5 sticky top-0 right-0 z-1000  text-custom-color rounded-2xl m-[1em] text-2xl cursor-pointer'>
         {/* mobile-view */}
         <div className='px-2 min-[980px]:hidden'>
           <div className='flex justify-between items-center '>
@@ -108,7 +109,11 @@ export default function Navbar() {
           <ul className='flex gap-3 items-center'>
             <li>
               <span className=''>
-                <Search />
+                <Search
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  placeholder='Try search here..'
+                />
               </span>
             </li>
             <li>
