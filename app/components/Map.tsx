f4da99f57cac9a82ee0837246d042ab630e3a646
muritador5050 <muritador5050@gmail.com>
2025-04-10 07:11:03 +0100

    i gave gap to map container

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -3,10 +3,10 @@ import { Progress, AspectRatio } from '@chakra-ui/react';
 
 export default function MapComponent() {
   return (
-    <div className='flex flex-col justify-between bg-custom-bg rounded-xl px-3 py-5 mx-auto h-auto min-[980px]:w-[350px]'>
+    <div className='flex flex-col gap-7 bg-custom-bg rounded-xl px-3 py-5 mx-auto h-auto min-[980px]:w-[350px]'>
       {/* Map Section */}
-      <h1 className='text-xxl'>Visit From USA</h1>
-      <div className='h-64 md:h-1/2 mt-3 mb-15'>
+      <div className=''>
+        <h1 className='text-xxl'>Visit From USA</h1>
         <AspectRatio ratio={3 / 2}>
           <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.952912260219!2d3.375295414770757!3d6.5276316452784755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1567723392506!5m2!1sen!2sng' />
         </AspectRatio>
@@ -20,7 +20,10 @@ export default function MapComponent() {
           { name: 'KA', value: 60 },
           { name: 'AZ', value: 80 },
         ].map((item, index) => (
-          <div key={index} className='flex items-center justify-between gap-3'>
+          <div
+            key={index}
+            className='flex items-center justify-between gap-3 mt-2'
+          >
             <p className='w-10 text-sm md:text-base'>{item.name}</p>
             <Progress
               size='sm'
